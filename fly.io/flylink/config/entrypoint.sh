463d8d4b5612bd67f6e2aea482d5fbb31121835b
Vijayakumar Ravi <im@vijayakumar.xyz>
2025-01-01 22:28:15 +0530

    :sparkles: feat: Added LINKDING fly.io config
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

---
+++ fly.io/flylink/config/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+
+echo -e "${GREEN}Info: Starting up...${NC}"
+
+mkdir -pv /data/tailscale
+
+# error: adding [-i tailscale0 -j MARK --set-mark 0x40000] in v4/filter/ts-forward: running [/sbin/iptables -t filter -A ts-forward -i tailscale0 -j MARK --set-mark 0x40000 --wait]: exit status 2: iptables v1.8.6 (legacy): unknown option "--set-mark"
+modprobe xt_mark
+
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+
+# Deleting old fly node from tailnet
+apikey=$(curl -s -d "client_id=${OAUTH_CLIENT_ID}" -d "client_secret=${OAUTH_CLIENT_SECRET}" "https://api.tailscale.com/api/v2/oauth/token" | jq .access_token | tr -d '"')
+targetname="${FLY_APP_NAME}"
+curl -s "https://api.tailscale.com/api/v2/tailnet/-/devices" -u "$apikey:" | jq -r '.devices[] |  "\(.id) \(.name)"' |
+  while read id name; do
+    if [[ $name = *"$targetname"* ]]
+    then
+      echo -e "${YELLOW}Warn: Found previous node($name) still in tailnet - getting rid of it${NC}"
+      curl -s -X DELETE "https://api.tailscale.com/api/v2/device/$id" -u "$apikey:"
+    fi
+  done
+
+# litestream database restore
+litestream restore -if-db-not-exists -if-replica-exists -config /etc/litestream.yaml /etc/linkding/data/db.sqlite3
+
+# Execute the CMD
+exec "$@"
