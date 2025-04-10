ad95ad367ae5eb34281a6cc9b4c6a91720d9d331
Vijayakumar Ravi <im@vijayakumar.xyz>
2025-04-10 07:23:44 +0530

    :hammer_and_wrench: chore(flake): flake update
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- flake.nix
+++ flake.nix
@@ -4,53 +4,40 @@
 
     talhelper.url = "github:budimanjojo/talhelper";
     talhelper.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Git pre-commit hooks
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs: let
     # Supported systems for NixOS and MacOS
     supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
     forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-
-    # Pre-commit hooks configuration for all systems
-    pre-commit = forAllSystems (system: {
-      pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          alejandra.enable = true;
-          actionlint.enable = true;
-          shellcheck.enable = true;
-          flake-checker.enable = true;
-          check-symlinks.enable = true;
-          end-of-file-fixer.enable = true;
-          detect-private-keys.enable = true;
-          trim-trailing-whitespace.enable = true;
-          trim-trailing-whitespace.stages = ["pre-commit"];
-        };
-      };
-    });
   in {
     # Development shell with pre-commit hooks
     devShells = forAllSystems (system: {
       default = inputs.nixpkgs.legacyPackages.${system}.mkShell {
-        inherit (pre-commit.${system}.pre-commit-check) shellHook;
-        buildInputs = with inputs.nixpkgs.legacyPackages.${system};
-          [
-            jq
-            git
-            flux
-            sops
-            go-task
-            kubectl
-            python3
-            helmfile
-            talosctl
-            minijinja
-            inputs.talhelper.packages.${system}.default
-          ]
-          ++ pre-commit.${system}.pre-commit-check.enabledPackages;
+        buildInputs = with inputs.nixpkgs.legacyPackages.${system}; [
+          jq
+          git
+          flux
+          sops
+          go-task
+          kubectl
+          python3
+          helmfile
+          talosctl
+          minijinja
+          inputs.talhelper.packages.${system}.default
+        ];
+        shellHook = ''
+          # Set environment variables based on the current directory as config root
+          export KUBERNETES_DIR="$(pwd)/kubernetes/luffy"
+          export SOPS_AGE_KEY_FILE="$(pwd)/age.agekey"
+          export TALOSCONFIG="$(pwd)/kubernetes/luffy/talos/clusterconfig/talosconfig"
+          # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+          # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+          export PIP_PREFIX=$(pwd)/_build/pip_packages
+          export PYTHONPATH="$PIP_PREFIX/${inputs.nixpkgs.legacyPackages.${system}.python313.sitePackages}:$PYTHONPATH"
+          export PATH="$PIP_PREFIX/bin:$PATH"
+          unset SOURCE_DATE_EPOCH
+        '';
       };
     });
   };
