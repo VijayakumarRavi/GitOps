72008116ad5b1518d9c7c2cac87eab0532828089
Vijayakumar Ravi <im@vijayakumar.xyz>
2025-06-04 05:00:14 +0530

    :hammer_and_wrench: chore(hcvault): autounseal with azure key vault
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- fly.io/hcvault/config/entrypoint.sh
+++ fly.io/hcvault/config/entrypoint.sh
@@ -16,17 +16,25 @@ api_addr = "${VAULT_ADDR}"
 cluster_addr = "${VAULT_ADDR}:8201"
 
 storage "s3" {
+  endpoint   = "${AWS_ENDPOINT}"
   access_key = "${AWS_ACCESS_KEY_ID}"
   secret_key = "${AWS_SECRET_ACCESS_KEY}"
   bucket     = "fly"
   path       = "hcvault/storage"
   s3_force_path_style = "true"
-  endpoint   = "${AWS_ENDPOINT}"
 }
 
 listener "tcp" {
   address     = "[::]:8200"
   tls_disable = "true"
+}
+
+seal "azurekeyvault" {
+  client_id      = "${AZURE_CLIENT_ID}"
+  client_secret  = "${AZURE_CLIENT_SECRET}"
+  tenant_id      = "${AZURE_TENANT_ID}"
+  vault_name     = "hcvault-e5cb030c"
+  key_name       = "hcvault"
 }
 EOF
 echo -e "${GREEN}Info: Vault configuration written to /config.hcl${NC}"
